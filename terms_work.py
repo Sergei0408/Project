4269ce1fa9a872f8b43af9775bff2dacceb9c664
Sergei0408 <132344558+Sergei0408@users.noreply.github.com>
2023-05-03 17:38:34 +0300

    Add files via upload

--- terms_work.py
+++ terms_work.py
@@ -1,6 +1,6 @@
 def get_terms_for_table():
     terms = []
-    with open("./data/terms.csv", "r", encoding="utf-8") as f:
+    with open("terms.csv", "r", encoding="utf-8") as f:
         cnt = 1
         for line in f.readlines()[1:]:
             term, definition, source = line.split(";")
@@ -11,44 +11,12 @@ def get_terms_for_table():
 
 def write_term(new_term, new_definition):
     new_term_line = f"{new_term};{new_definition};user"
-    with open("./data/terms.csv", "r", encoding="utf-8") as f:
+    with open("terms.csv", "r", encoding="utf-8") as f:
         existing_terms = [l.strip("\n") for l in f.readlines()]
         title = existing_terms[0]
         old_terms = existing_terms[1:]
     terms_sorted = old_terms + [new_term_line]
     terms_sorted.sort()
     new_terms = [title] + terms_sorted
-    with open("./data/terms.csv", "w", encoding="utf-8") as f:
+    with open("terms.csv", "w", encoding="utf-8") as f:
         f.write("\n".join(new_terms))
-
-def terms_list(request):
-    terms = terms_work.get_terms_for_table()
-    return render(request, "term_list.html", context={"terms": terms})
-
-
-def add_term(request):
-    return render(request, "term_add.html")
-
-
-def send_term(request):
-    if request.method == "POST":
-        cache.clear()
-        user_name = request.POST.get("name")
-        new_term = request.POST.get("new_term", "")
-        new_definition = request.POST.get("new_definition", "").replace(";", ",")
-        context = {"user": user_name}
-        if len(new_definition) == 0:
-            context["success"] = False
-            context["comment"] = "Описание должно быть не пустым"
-        elif len(new_term) == 0:
-            context["success"] = False
-            context["comment"] = "Термин должен быть не пустым"
-        else:
-            context["success"] = True
-            context["comment"] = "Ваш термин принят"
-            terms_work.write_term(new_term, new_definition)
-        if context["success"]:
-            context["success-title"] = ""
-        return render(request, "term_request.html", context)
-    else:
-        add_term(request)
--- views.py
+++ views.py
@@ -2,6 +2,8 @@ from django.conf import settings
 import telebot
 import settings
 import os
+import terms_work
+from telebot import types
 
 TOKEN = settings.TELEGRAM_TOKEN
 
@@ -12,13 +14,65 @@ def greet(message):
     bot.send_message(message.chat.id, '''Здравствуйте, {0.first_name}!
 {1.first_name} - бот для изучения иностранного языка. 
 Давайте начнем!'''.format(message.from_user, bot.get_me()))
+    markup = types.InlineKeyboardMarkup(resize_keyboard=True)
+    item1 = types.InlineKeyboardButton("Записать слово", zapis)
+    item2 = types.InlineKeyboardButton("Вспомнить слово", chtenie)
+    markup.add(item1,item2)
+    bot.send_message(message.chat.id, 'Начинаем', reply_markup=service)
 
 
 @bot.message_handler(content_types=['text'])
 def dictionary(message):
-    
-    bot.register_next_step_handler(new_term, new_definition)
-    term_works.write_term(new_term, new_definition)
+    '''
+    dictionary(message)
+    print(message)
+    print(message.text)
+    s = message.text.split(' - ')
+    new_term = s[0]
+    new_definition = s[1]
+    print(s[1])
+    terms_work.write_term(new_term, new_definition)
+    print(message.text)
+    qw = message.txt
+    qw = qw.replac('=', '')
+    terms = terms_work.get_terms_for_table()
+    q = [i for i, (_, x, _) in enumerate(terms) if x == qw]
+    print(q)
+    bot.send_message(message.chat.id, terms[q][2])
+    '''
+
+    if '-' in message.text:
+        dictionary(message)
+        print(message)
+        print(message.text)
+        s = message.text.split(' - ')
+        new_term = s[0]
+        new_definition = s[1]
+        print(s[1])
+        terms_work.write_term(new_term, new_definition)
+
+    elif '=' in message.text:
+        print(message.text)
+        qw = message.txt
+        qw = qw.replac('=','')
+        terms  = terms_work.get_terms_for_table()
+        q = [i for i, (_, x, _) in enumerate(terms) if x == qw]
+        print(q)
+        bot.send_message(message.chat.id, terms[q][2])
+
+'''
+@bot.message_handler(func=lambda message: message == "действие")
+def reply_message_handler(message):
+    bot.send_message(message.chat.id, 'Выберите действие', reply_markup=markup)
+'''
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+        if call.message:
+            if call.data == 'zapis':
+                bot.send_message(call.message.chat.id, 'Запишите слово и определение через знак -')
+            elif call.data == 'chtenie':
+                bot.send_message(call.message.chat.id, 'Какое слово Вы ищите? Воспользуйтесь знаком = ')
 
 '''
 from django.core.exceptions import PermissionDenied
@@ -33,4 +87,9 @@ def bothook(request):
         raise PermissionDenied  # ошибка 403. Мы таким образом защищаем бота от разнообразных атак.
 '''
 
-bot.polling(none_stop=True)
+while True:
+    try:
+        bot.polling(none_stop=True)
+
+    except Exception as e:
+        print(e)
