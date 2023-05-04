a2d989e40cb2ec6106f7cca9f98148ff3cb624aa
Sergei0408 <132344558+Sergei0408@users.noreply.github.com>
2023-05-04 03:25:22 +0300

    Add files via upload

--- terms_work.py
+++ terms_work.py
@@ -20,3 +20,24 @@ def write_term(new_term, new_definition):
     new_terms = [title] + terms_sorted
     with open("terms.csv", "w", encoding="utf-8") as f:
         f.write("\n".join(new_terms))
+
+def write_date(time_now):
+    new_date_line = f"{time_now};user"
+    with open("date.csv", "r", encoding="utf-8") as f:
+        existing_date = [l.strip("\n") for l in f.readlines()]
+        title = existing_date[0]
+        old_date = existing_date[1:]
+    date_sorted = old_date + [new_date_line]
+    new_date = [title] + date_sorted
+    with open("date.csv", "w", encoding="utf-8") as f:
+        f.write("\n".join(new_date))
+
+def get_date_for_table():
+    date = []
+    with open("date.csv", "r", encoding="utf-8") as f:
+        cnt = 1
+        for line in f.readlines()[1:]:
+            time, source = line.split(";")
+            date.append([cnt, time])
+            cnt += 1
+    return date
--- views.py
+++ views.py
@@ -4,6 +4,7 @@ import settings
 import os
 import terms_work
 from telebot import types
+import time
 
 TOKEN = settings.TELEGRAM_TOKEN
 
@@ -14,52 +15,67 @@ def greet(message):
     bot.send_message(message.chat.id, '''Здравствуйте, {0.first_name}!
 {1.first_name} - бот для изучения иностранного языка. 
 Давайте начнем!'''.format(message.from_user, bot.get_me()))
-    markup = types.InlineKeyboardMarkup(resize_keyboard=True)
-    item1 = types.InlineKeyboardButton("Записать слово", zapis)
-    item2 = types.InlineKeyboardButton("Вспомнить слово", chtenie)
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+
+    item1 = types.KeyboardButton("Запомнить слово")
+    item2 = types.KeyboardButton("Вспомнить слово")
     markup.add(item1,item2)
-    bot.send_message(message.chat.id, 'Начинаем', reply_markup=service)
+
+    bot.send_message(message.chat.id, "Начали".format(message.from_user, bot.get_me()), parse_mode='html',
+                     reply_markup=markup)
+   # bot.send_message(message.chat.id, 'Начинаем', reply_markup=markup)
 
 
 @bot.message_handler(content_types=['text'])
 def dictionary(message):
-    '''
-    dictionary(message)
-    print(message)
     print(message.text)
-    s = message.text.split(' - ')
-    new_term = s[0]
-    new_definition = s[1]
-    print(s[1])
-    terms_work.write_term(new_term, new_definition)
-    print(message.text)
-    qw = message.txt
-    qw = qw.replac('=', '')
-    terms = terms_work.get_terms_for_table()
-    q = [i for i, (_, x, _) in enumerate(terms) if x == qw]
-    print(q)
-    bot.send_message(message.chat.id, terms[q][2])
-    '''
-
-    if '-' in message.text:
-        dictionary(message)
-        print(message)
-        print(message.text)
-        s = message.text.split(' - ')
-        new_term = s[0]
-        new_definition = s[1]
-        print(s[1])
+    sms = message.text
+    if sms == 'Запомнить слово':
+        bot.send_message(message.chat.id, "Введите определение через знак -")
+    elif sms == 'Вспомнить слово':
+        bot.send_message(message.chat.id, "Введите слово и добавьте знак вопроса (в формате: слово? )")
+    elif sms == "Записать время занятия":
+        seconds = time.time()
+        local_time = time.ctime(seconds)
+        terms_work.write_date(local_time)
+    elif sms == "Просмотреть время занятий":
+        date = terms_work.get_date_for_table()
+        all_time = []
+        for x in date:
+            all_time.append(x[1])
+        bot.send_message(message.chat.id, "\n".join(all_time))
+    elif '-' in sms:
+        s = sms.split(' - ')
+        new_term = s[0].lower()
+        new_definition = s[1].lower()
         terms_work.write_term(new_term, new_definition)
-
-    elif '=' in message.text:
-        print(message.text)
-        qw = message.txt
-        qw = qw.replac('=','')
+        bot.send_message(message.chat.id, "Выберите следующее действие")
+    elif '?' in sms:
+        qw = sms.replace('?','').lower()
         terms  = terms_work.get_terms_for_table()
+        print(qw)
         q = [i for i, (_, x, _) in enumerate(terms) if x == qw]
-        print(q)
-        bot.send_message(message.chat.id, terms[q][2])
+        try:
+            n = int(q[0])
+            print(terms[n][2])
+            bot.send_message(message.chat.id, terms[n][2])
+        except Exception as e:
+            bot.send_message(message.chat.id, 'Такого слова в словаре нет')
 
+    else:
+        bot.send_message(message.chat.id, 'Неизвестный запрос')
+
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1 = types.KeyboardButton("Запомнить слово")
+    item2 = types.KeyboardButton("Вспомнить слово")
+    item3 = types.KeyboardButton("Записать время занятия")
+    item4 = types.KeyboardButton("Просмотреть время занятий")
+    markup.add(item1, item2)
+    markup.add(item3, item4)
+
+    if '-' not in sms and sms !='Вспомнить слово' and sms !='Запомнить слово':
+        bot.send_message(message.chat.id, "Выберите следующее действие".format(message.from_user, bot.get_me()), parse_mode='html',
+                     reply_markup=markup)
 '''
 @bot.message_handler(func=lambda message: message == "действие")
 def reply_message_handler(message):
